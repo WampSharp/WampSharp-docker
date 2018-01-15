e56406148822f99cf98db74ae04ba80b9ea450ca
Elad Zelingher <elad@codesharp.net>
2018-01-14 19:27:21 -0500

    Initial commit

---
+++ client/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using WampSharp.V2;
+using WampSharp.V2.Client;
+using WampSharp.V2.Fluent;
+
+namespace MyApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string url = "ws://127.0.0.1:8080/ws/";
+            string realm = "realm1";
+
+            WampChannelFactory channelFactory = new WampChannelFactory();
+
+            IWampChannel channel =
+                channelFactory.ConnectToRealm(realm)
+                    .WebSocketTransport(new Uri(url))
+                    .JsonSerialization()
+                    .Build();
+
+            // Fire this code when the connection establishes
+            channel.RealmProxy.Monitor.ConnectionEstablished += (sender, e) =>
+            {
+                Console.WriteLine("Session open! :" + e.SessionId);
+            };
+
+            // Fire this code when the connection has broken
+            channel.RealmProxy.Monitor.ConnectionBroken += (sender, e) =>
+            {
+                Console.WriteLine("Session closed! :" + e.Reason);
+            };
+
+            await channel.Open().ConfigureAwait(false);
+
+            // Your code goes here: use WAMP via the channel variable to
+            // call, register, subscribe and publish ..
+
+            await Task.Delay(1000);
+        }
+    }
+}
---
+++ router/Startup.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using WampSharp.AspNetCore.WebSockets.Server;
+using WampSharp.Binding;
+using WampSharp.V2;
+
+namespace MyApp
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            WampHost host = new WampHost();
+
+            app.Map("/ws", builder =>
+            {
+                builder.UseWebSockets();
+
+                host.RegisterTransport(new AspNetCoreWebSocketTransport(builder),
+                    new JTokenJsonBinding());
+            });
+
+            app.Map("/test", builder =>
+            {
+                builder.Run(async context => await context.Response.WriteAsync("Hello World!"));
+            });
+
+            host.Open();
+        }
+    }
+}
